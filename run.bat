a10ac7e8ee5a4118b6e8e32651ff5993658a6e53
npcBerk <muhammetberk561@gmail.com>
2025-04-13 03:37:06 +0300

    Configure allure result's output path

--- run.bat
+++ run.bat
@@ -3,6 +3,6 @@ echo ================================
 echo  Allure Raporlu Behave Testleri
 echo ================================
 
-poetry run behave tests/features -f allure_behave.formatter:AllureFormatter -o allure-results
-allure generate allure-results -o allure-report --clean
-allure open allure-report
+poetry run behave tests/features -f allure_behave.formatter:AllureFormatter -o tests/allure-results
+allure generate tests/allure-results -o tests/allure-report --clean
+allure open tests/allure-report
